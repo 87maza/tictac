555734781333160af0c3a764915d05d72050e0d5
87maza <87maza@gmail.com>
2016-04-04 23:28:04 -0700

    added some win logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var nextTurn ="X";
+var p1icon = "X"
+var nextTurn = p1icon;
 var p2icon = "O";
 var square = document.getElementsByClassName('square');
 for( var i = 0; i<square.length; i++){
@@ -17,12 +18,22 @@ function addIcon(){
 };
 
  function changeTurn(){
-      if(nextTurn === 'X'){
+      if(nextTurn === p1icon){
            nextTurn = p2icon;
       } else {
-           nextTurn = 'X';
+           nextTurn = p1icon;
       }
  }
-function winner() {
-  
+function checkWinner() {
+  //use square[i] array of arrays!!
+  var winningCombos = [[square[0],square[1],square[2]], [square[3],square[4],square[5]], [square[6],square[7],square[8]], 
+                       [square[0],square[4],square[8]], [square[2],square[4],square[6]], [square[0],square[3],square[6]], 
+                       [square[1],square[4],square[7]], [square[2],square[5],square[8]]
+                       ];
+  for (var j = 0; j < 8; j++){
+    for(var k =0; k < 3; k++){
+      var windex = winningCombos[j][k];
+
+    }
+  }
 }
