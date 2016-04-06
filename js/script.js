1910e5b583281cb29aae6d7f7cef160936a06451
87maza <87maza@gmail.com>
2016-04-05 22:20:41 -0700

    win logic works, need to add computer ai

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var nextTurn = p1icon;
 var p2icon = "O";
 var square = document.getElementsByClassName('square');
 for( var i = 0; i<square.length; i++){
+  square[i].squareVal=0;
   square[i].addEventListener('click', addIcon);
 }
 function addIcon(){
@@ -11,29 +12,44 @@ function addIcon(){
     return;
   }
   else{
-    console.log(nextTurn);
     this.innerHTML="<p class='marked'>" + nextTurn + "</p>";
+    if(nextTurn === p1icon){
+      this.squareVal = 1;
+    }
+    else if(nextTurn === p2icon){
+      this.squareVal= -1;
+    }
     changeTurn();
   }  
 };
-
  function changeTurn(){
       if(nextTurn === p1icon){
-           nextTurn = p2icon;
+          nextTurn = p2icon;
+          checkWinner();
       } else {
            nextTurn = p1icon;
+           checkWinner();
       }
- }
+  }
 function checkWinner() {
-  //use square[i] array of arrays!!
-  var winningCombos = [[square[0],square[1],square[2]], [square[3],square[4],square[5]], [square[6],square[7],square[8]], 
-                       [square[0],square[4],square[8]], [square[2],square[4],square[6]], [square[0],square[3],square[6]], 
-                       [square[1],square[4],square[7]], [square[2],square[5],square[8]]
-                       ];
-  for (var j = 0; j < 8; j++){
-    for(var k =0; k < 3; k++){
-      var windex = winningCombos[j][k];
-
-    }
+  var winningCombos = [
+    [square[0].squareVal,square[1].squareVal,square[2].squareVal], 
+    [square[3].squareVal,square[4].squareVal,square[5].squareVal], 
+    [square[6].squareVal,square[7].squareVal,square[8].squareVal], 
+    [square[0].squareVal,square[4].squareVal,square[8].squareVal], 
+    [square[2].squareVal,square[4].squareVal,square[6].squareVal], 
+    [square[0].squareVal,square[3].squareVal,square[6].squareVal], 
+    [square[1].squareVal,square[4].squareVal,square[7].squareVal], 
+    [square[2].squareVal,square[5].squareVal,square[8].squareVal]
+  ];
+  for(var j=0; j<7; j++){
+    var checker = winningCombos[j].reduce(function(a,b){return a+b});
+    if (checker === 3) {
+      console.log('x winner');
+    };
+    if(checker === -3) {
+      console.log('o winner');
+    };
   }
 }
+
